8071f5f8ff1c7456dcc2125467fe2d5012072551
Farren Esman <farren.esman@gmail.com>
2021-04-02 18:15:51 +1100

    styles and html

--- src/components/FancySquare.js
+++ src/components/FancySquare.js
@@ -2,15 +2,27 @@ import React from "react"
 import { Link } from "gatsby"
 import styles from '../styles/FancySquare.module.scss'
 
-const FancySquare = ({ post }) => (
-  <article className={styles.FancySquare}>
-    <div className={styles.inner}>
-      <ul className={styles.ul}>
-        <li>Title: {post.frontmatter.title}</li>
-        <li>Date: {post.frontmatter.date}</li>
-      </ul>
-    </div>
-    {/* <span className={styles.close}>x</span> */}
-  </article>
-)
+const FancySquare = ({ post }) => {
+  console.log(`pinned? ${post.frontmatter.pinned}`);
+  return (
+    <article
+      className={`
+        ${styles.FancySquare} 
+        ${post.frontmatter.pinned ? 'pinned' : ''}
+      `}
+    >
+      <div className={styles.inner}>
+        <h1>
+          {post.frontmatter.title} {post.frontmatter.pinned}
+        </h1>
+        <div className={styles.footer}>
+          <span className={styles.date}>
+            {post.frontmatter.date}
+          </span>
+        </div>
+      </div>
+      {/* <span className={styles.close}>x</span> */}
+    </article>
+  )
+}
 export default FancySquare
