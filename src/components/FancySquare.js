badfd603f0ee975cd9572ee55458aa7be1751cca
Farren Esman <farren.esman@gmail.com>
2021-04-02 18:36:04 +1100

    icons for fancy square

--- src/components/FancySquare.js
+++ src/components/FancySquare.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 import styles from '../styles/FancySquare.module.scss'
+import { FaEllipsisH } from 'react-icons/fa'
+import { BiLinkExternal } from 'react-icons/bi'
 
 const FancySquare = ({ post }) => {
   console.log(`pinned? ${post.frontmatter.pinned}`);
@@ -12,9 +14,25 @@ const FancySquare = ({ post }) => {
       `}
     >
       <div className={styles.inner}>
-        <h1>
-          {post.frontmatter.title} {post.frontmatter.pinned}
-        </h1>
+        <div className={styles.header}>
+          <h1>
+            {post.frontmatter.title} {post.frontmatter.pinned}
+          </h1>
+        </div>
+        <div className={styles.body}>
+          <Link
+            to={post.frontmatter.path}
+            className={styles.link}
+          >
+            <FaEllipsisH />
+          </Link>
+          <Link
+            to={post.frontmatter.path}
+            className={styles.link}
+          >
+            <BiLinkExternal />
+          </Link>
+        </div>
         <div className={styles.footer}>
           <span className={styles.date}>
             {post.frontmatter.date}
