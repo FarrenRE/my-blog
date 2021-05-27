88d95561679e297904eb5d5db71d9935bd8966eb
Farren Esman <farren.esman@gmail.com>
2021-05-27 20:56:06 +1000

    feature: useTrail animation for FancySquare. NOTE: FancySquare now wrapped with animated.div to circumvent custom component issue with useTrail

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,8 @@ import React from "react"
 import Helmet from 'react-helmet'
 import { graphql } from 'gatsby'
 import FancySquare from '../components/FancySquare'
-// import { useTrail } from 'react-spring/renderprops'
+import { animated, useTrail } from 'react-spring'
+import styles from '../styles/index.module.scss'
 
 const IndexPage = ({
   data: {
@@ -11,14 +12,18 @@ const IndexPage = ({
   },
 }) => {
 
-  const Posts = edges
-    .map(
-      edge => 
-        <FancySquare 
-          key={edge.node.id} 
-          post={edge.node} 
-        />
-      )
+  console.log(`edges.length = ${edges.length}`);
+  const trail = useTrail(edges.length, {
+    from: {
+      opacity: 0,
+      left: '-20px'
+    },
+    to: { 
+      opacity: 1,
+      left: '0'
+    }
+  })
+  console.log(trail)
 
   return (
     <>
@@ -26,11 +31,22 @@ const IndexPage = ({
         <title>{site.siteMetadata.title}</title>
         <meta name="description" content={site.siteMetadata.description} />
       </Helmet>
-      {/* {trail.map(({ opacity }, index) => (
-        items[index]
-      ))} */}
       <div className="squares">
-        {Posts}
+        {trail.map( ( props, i ) => (
+          <>
+            <animated.div
+              className={ styles.animContainer }
+              style={ props }
+            >
+              <FancySquare 
+                key={edges[i].node.id} 
+                post={edges[i].node} 
+
+              />
+            </animated.div>
+            
+          </>
+        ) )}
       </div>
     </>
   )
@@ -45,7 +61,7 @@ query indexPageQuery {
       description
     }
   }
-  allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}, filter: {frontmatter: {draft: {ne: true}}}) {
+  allMarkdownRemark(sort: {order: [ASC, DESC], fields: [frontmatter___order, frontmatter___date]}, filter: {frontmatter: {draft: {ne: true}}}) {
     edges {
       node {
         id
